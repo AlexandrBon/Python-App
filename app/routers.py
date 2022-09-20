40b189c6d0a212ae467615c0311990560be7e425
Alexandr Bondarenko <bon527870@mail.ru>
2022-09-20 11:50:02 +0300

    remove extra function

--- app/routers.py
+++ app/routers.py
@@ -22,20 +22,6 @@ async def read_user(user_id: str, q: str | None = None):
     return {"item_id": user_id}
 
 
-@router.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(  # noqa: D103
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-
 @router.post("/items/")
 async def create_item(item: contracts.Item):  # noqa: D103
     item_dict = item.dict()
